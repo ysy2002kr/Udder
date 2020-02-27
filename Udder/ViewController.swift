d466a4411266667ee4d5afc14b2c2452210f1927
Soyeon <ysy2002kr@hotmail.com>
2020-02-27 20:51:32 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Soyeon Yoon on 2020/02/27.
+//  Copyright © 2020 Trinder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
